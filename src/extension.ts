d0752c8b8fd3df818531eabfdb1435b934a17da7
jekky <11986158+jac3km4@users.noreply.github.com>
2022-07-01 00:07:28 +0100

    Bump

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ import path = require('path');
 
 let client: LanguageClient;
 
-const version = "v0.0.14";
+const version = "v0.1.0";
 const exeDownloadUrl = `https://github.com/jac3km4/redscript-ide/releases/download/${version}/redscript-ide.exe`;
 
 export async function activate(context: ExtensionContext) {
